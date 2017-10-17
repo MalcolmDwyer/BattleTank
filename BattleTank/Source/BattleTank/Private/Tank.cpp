84250c95d33cbce3dfc3921564b904c6107e9c3c
Malcolm Dwyer <malcolm.dwyer@gmail.com>
2017-10-16 19:29:05 -0500

    Spawning (still) projectiles at end of barrel on Fire()

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -1,6 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Tank.h"
+#include "TankBarrel.h"
+#include "Engine/StaticMeshSocket.h"
+#include "Projectile.h"
 #include "TankAimingComponent.h"
 
 
@@ -35,6 +38,7 @@ void ATank::AimAt(FVector HitLocation) {
 void ATank::SetBarrelReference(UTankBarrel* BarrelToSet)
 {
   TankAimingComponent->SetBarrelReference(BarrelToSet);
+  Barrel = BarrelToSet;
 }
 
 void ATank::SetTurretReference(UTankTurret* TurretToSet)
@@ -47,4 +51,13 @@ void ATank::Fire()
 {
   auto Time = GetWorld()->GetTimeSeconds();
   UE_LOG(LogTemp, Warning, TEXT("%f: Fire"), Time)
+  
+  if (!Barrel) { return; }
+
+  // Spawn a projectile at barrel location
+  GetWorld()->SpawnActor<AProjectile>(
+                                      ProjectileBlueprint,
+                                      Barrel->GetSocketLocation(FName("Projectile")),
+                                      Barrel->GetSocketRotation(FName("Projectile"))
+                                      );
 }
