4b81f1dafc8e9112b62eea30decdc3cdba8c8e6c
Malcolm Dwyer <malcolm.dwyer@gmail.com>
2017-10-19 20:34:32 -0500

    Tank reticle colors organized in blueprint (but not actually being set yet)

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -6,6 +6,15 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
+// Enum for aiming state
+UENUM()
+enum class EFiringState : uint8
+{
+  Reloading,
+  Aiming,
+  Locked
+};
+
 // Forward declaration
 class UTankBarrel;
 class UTankTurret;
@@ -23,12 +32,17 @@ public:
   void SetBarrelReference(UTankBarrel* BarrelToSet);
   void SetTurretReference(UTankTurret* TurretToSet);
 
-public:
   void AimAt(FVector HitLocation, float LaunchSpeed);
+  
+protected:
+  UPROPERTY(BlueprintReadonly, Category = "State")
+  EFiringState FiringState = EFiringState::Reloading;
 
 private:
   UTankBarrel* Barrel = nullptr;
   UTankTurret* Turret = nullptr;
+
+
   
   void MoveBarrelTowards(FVector AimDirection);
   
