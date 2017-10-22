9b797dcd6331a02e261ddb27ad48fb928bd14a21
Malcolm Dwyer <malcolm.dwyer@gmail.com>
2017-10-21 22:05:05 -0500

    Avoiding unreal bug that crashed when opening Player Controller BP. https://issues.unrealengine.com/issue/UE-36929 https://community.gamedev.tv/t/tankplayercontroller-bp-unresolved-compiler-error/18079

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -20,6 +20,7 @@ void ATankPlayerController::Tick(float DeltaTime)
 
 void ATankPlayerController::AimTowardsCrossHair()
 {
+  if (!GetPawn()) { return; }
   auto AimingComponent = GetPawn()->FindComponentByClass<UTankAimingComponent>();
   if (!ensure(AimingComponent)) { return; }
   
